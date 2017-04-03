66d123d64da17f167805d88ffced7e09085cbaa0
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-04-04 01:56:11 +0200

    fix(theme): phpcs compliance

--- theme/functions/functions-loader.php
+++ theme/functions/functions-loader.php
@@ -6,7 +6,7 @@
  */
 
 // Make sure this file is called by wp.
-defined( 'ABSPATH' ) or die();
+defined( 'ABSPATH' ) || die();
 
 $files = array_diff( glob( __DIR__ . DIRECTORY_SEPARATOR . '*.php' ), array( __FILE__ ) );
 foreach ( $files as $filename ) {
